c3902178666194bdedfdbf5ef8d3a22b54def1ac
wintertee <wintertee@foxmail.com>
2020-03-12 20:50:14 +0800

    complete file tree

--- utils/FileTree.py
+++ utils/FileTree.py
@@ -11,12 +11,18 @@ class FileTree():
         if dir_level >= 0:
             self.__current_level = dir_level
             self.__last_is_file = False
-            print("│   " * self.__current_level + "├───" + color + str + '/' + Style.RESET_ALL)
+            if self.__current_level == 0:
+                print(color + str + '/' + Style.RESET_ALL)
+            else:
+                print("│   " * (self.__current_level-1) + "├───" + color + str + '/' + Style.RESET_ALL)
         else:
             if not self.__last_is_file:
                 self.__current_level += 1
             self.__last_is_file = True
-            print("│   " * self.__current_level + "├───" + color + str + Style.RESET_ALL)
+            if self.__current_level == 0:
+                print(color + str + Style.RESET_ALL)
+            else:
+                print("│   " * (self.__current_level-1) + "├───" + color + str + Style.RESET_ALL)
 
 
 file_tree = FileTree()
